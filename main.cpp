4019517613b51a9d70079b327ee031bd0ba97991
dmitrienkoae <54313684+dmitrienkoae@users.noreply.github.com>
2020-04-14 20:21:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,18 @@
 #include "solution.hpp"
 
 int main() {
-
-
 	std::vector<std::string> vs;
 	std::string s;
-	while(std::cin >> s) {
-		vs.push_back(s);
+	std::string line;
+	std::ifstream in("D:\\tests.txt"); \\input file
+	if (in.is_open())
+	{
+		while (getline(in, line))
+		{
+			vs.push_back(line);
+		}
 	}
+	in.close();
 
 	std::cout << super_string(vs) << std::endl;
 return 0;

b55826d736f14e29230897c5ed4855548901dd64
dmitrienkoae <54313684+dmitrienkoae@users.noreply.github.com>
2020-04-17 22:49:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <fstream>
 #include "solution.hpp"
 
 int main() {
 	std::vector<std::string> vs;
 	std::string s;
 	std::string line;
-	std::ifstream in("D:\\tests.txt"); \\input file
+	std::ifstream in("D:\\tests.txt"); 
 	if (in.is_open())
 	{
 		while (getline(in, line))
@@ -18,6 +18,10 @@ int main() {
 	}
 	in.close();
 
+	OverlapGraph g(vs);
+	//std::cout << g << std::endl;
+	std::cout << super_string(vs).size() << std::endl;
 	std::cout << super_string(vs) << std::endl;
 return 0;
 }
+
